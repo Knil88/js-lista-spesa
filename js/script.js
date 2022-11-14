4192d903c45a117ea360440036b1d4ba1de0d600
Yari <narducciyarigmail.com>
2022-11-14 17:56:32 +0100

    ricreazione ciclo for e while

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
 //Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
 //Creiamo la nostra lista array
-let myList = ["Pane","Latte","Uova","Riso"];
-let myButton = document.querySelector("button");
+let expense = ["pane ","pasta ","olio ","riso "];
+
+let expenseBtn = document.querySelector("button");
 let expenseList = document.querySelector(".list");
 
-// for(let i = 0; i < myList.length ; i++){
-//     let item = ` <div>${myList[i]}</div>`;
-//     expenseList.innerHTML += item ;
-
-    
-
+// creiamo prima il ciclo for
+// for(let i = 0;i<expense.length;i++ ){
+//     let item = `<div>${expense[i]}</div>`;
+//     expenseList.innerHTML += item;
 // }
+//Andiamo ora a creare il ciclo while
 let i = 0;
-while( i < myList.length ){
-    let item = ` <div>${myList[i]}</div>`;
-    expenseList.innerHTML += item ;
-    i++;
+while(i<expense.length){
+        let item = `<div>${expense[i]}</div>`;
+         expenseList.innerHTML += item;
+         i++ ;}
 
-    
-
-}
-myButton.addEventListener("click",
-function(){
-    let more = document.querySelector("#more").value;
-    myList.push(more);
-    expenseList.innerHTML += `<div>${more}</div>`;
-}
-)
+         //creiamo ora l'ev
 
